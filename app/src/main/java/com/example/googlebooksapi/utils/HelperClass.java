194a12d0d81c0d129c8ed6d5f6e9f12bc9135ca6
Hasan Qadir <hasan.qadir@gmail.com>
2020-12-06 22:51:46 +0500

    Removed redundant query parameter helper function.

--- app/src/main/java/com/example/googlebooksapi/utils/HelperClass.java
+++ app/src/main/java/com/example/googlebooksapi/utils/HelperClass.java
@@ -8,11 +8,4 @@ public class HelperClass {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
-
-    public static String queryParamFormatter(String volumeName, String authorName){
-        volumeName = volumeName.trim().replaceAll(" ", "+");
-        authorName = authorName.trim().replaceAll(" ", "+");
-        String queryParams = volumeName + "+" + Constants.AUTHOR_TAG + authorName;
-        return queryParams;
-    }
 }

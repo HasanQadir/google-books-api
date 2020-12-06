59f5ecb32d1bfbe5ed5a9a4436061c73be80acb5
Hasan Qadir <hasan.qadir@gmail.com>
2020-12-06 21:13:31 +0500

    Use query parameters for the API.

--- app/src/main/java/com/example/googlebooksapi/server/IWebService.java
+++ app/src/main/java/com/example/googlebooksapi/server/IWebService.java
@@ -4,8 +4,9 @@ import com.example.googlebooksapi.models.JsonFeed;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface IWebService {
     @GET("v1/volumes")
-    Call<JsonFeed> getBookVolume();
+    Call<JsonFeed> getBookVolume(@Query("q") String queryParams);
 }
